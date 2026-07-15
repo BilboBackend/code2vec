9a890e1a08512ba0c0de7937e329c330108b00c0
Oskar Hovmoller Dinesen <ohodi@dtu.dk>
2026-07-15 17:11:35 +0200

    Rearranged project to integrate with simbench as external python package

--- src/extractor.py
+++ src/extractor.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 
@@ -9,9 +10,22 @@ class Extractor:
         self.jar_path = jar_path
 
     def extract_paths(self, path):
-        command = ['java', '-cp', self.jar_path, 'JavaExtractor.App', '--max_path_length',
-                   str(self.max_path_length), '--max_path_width', str(self.max_path_width), '--file', path, '--no_hash']
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        command = [
+            "java",
+            "-cp",
+            self.jar_path,
+            "JavaExtractor.App",
+            "--max_path_length",
+            str(self.max_path_length),
+            "--max_path_width",
+            str(self.max_path_width),
+            "--file",
+            path,
+            "--no_hash",
+        ]
+        process = subprocess.Popen(
+            command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         out, err = process.communicate()
         output = out.decode().splitlines()
         if len(output) == 0:
@@ -20,20 +34,22 @@ class Extractor:
         hash_to_string_dict = {}
         result = []
         for i, line in enumerate(output):
-            parts = line.rstrip().split(' ')
+            parts = line.rstrip().split(" ")
             method_name = parts[0]
             current_result_line_parts = [method_name]
             contexts = parts[1:]
-            for context in contexts[:self.config.MAX_CONTEXTS]:
-                context_parts = context.split(',')
+            for context in contexts[: self.config.MAX_CONTEXTS]:
+                context_parts = context.split(",")
                 context_word1 = context_parts[0]
                 context_path = context_parts[1]
                 context_word2 = context_parts[2]
                 hashed_path = str(self.java_string_hashcode(context_path))
                 hash_to_string_dict[hashed_path] = context_path
-                current_result_line_parts += ['%s,%s,%s' % (context_word1, hashed_path, context_word2)]
-            space_padding = ' ' * (self.config.MAX_CONTEXTS - len(contexts))
-            result_line = ' '.join(current_result_line_parts) + space_padding
+                current_result_line_parts += [
+                    "%s,%s,%s" % (context_word1, hashed_path, context_word2)
+                ]
+            space_padding = " " * (self.config.MAX_CONTEXTS - len(contexts))
+            result_line = " ".join(current_result_line_parts) + space_padding
             result.append(result_line)
         return result, hash_to_string_dict
 
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from code2vec!")
+
+
+if __name__ == "__main__":
+    main()
